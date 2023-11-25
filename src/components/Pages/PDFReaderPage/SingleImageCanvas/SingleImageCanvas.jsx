85f306c779e99f5de10a0aa40216175c45d46f76
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-25 20:41:24 +0300

    implement text selection

--- src/components/Pages/PDFReaderPage/SingleImageCanvas/SingleImageCanvas.jsx
+++ src/components/Pages/PDFReaderPage/SingleImageCanvas/SingleImageCanvas.jsx
@@ -1,28 +1,37 @@
 import React, { useEffect, useRef } from "react";
 import classes from "./SingleImageCanvas.module.css";
+import WordDrawer from "../../../../services/WordService";
+import WordSelector from "./WordSelector";
 
 const SingleImageCanvas = ({ obj, scale }) => {
-  const canvasRef = useRef(null);
+  const imageCanvasRef = useRef(null);
+  const drawingCanvasRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    context.imageSmoothingEnabled = false;
+    const imageCanvas = imageCanvasRef.current;
+    const drawingCanvas = drawingCanvasRef.current;
+    const imgCtx = imageCanvas.getContext("2d");
 
     const img = new Image();
     img.src = obj.img;
     img.onload = () => {
       const scaleFactor = scale / img.width;
-      const imgHeight = img.height * scaleFactor;
-      canvas.width = scale;
-      canvas.height = imgHeight;
-      context.drawImage(img, 0, 0, scale, Math.round(imgHeight));
+      const imgHeight = Math.round(img.height * scaleFactor);
+
+      imageCanvas.width = scale;
+      drawingCanvas.width = scale;
+      imageCanvas.height = imgHeight;
+      drawingCanvas.height = imgHeight;
+
+      imgCtx.drawImage(img, 0, 0, scale, imgHeight);
+      new WordSelector(drawingCanvas, obj, scaleFactor);
     };
   }, [obj, scale]);
 
   return (
     <div className={classes.canvasWrapper} id={"page" + obj.number_page}>
-      <canvas ref={canvasRef} />
+      <canvas ref={imageCanvasRef} className={classes.imageCanvas} />
+      <canvas ref={drawingCanvasRef} className={classes.drawingCanvas} />
     </div>
   );
 };
