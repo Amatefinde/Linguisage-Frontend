290f831508b89dbde1cb1bacbeb1923e35784508
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-11-26 12:41:51 +0300

    change zoom coef

---
+++ src/components/Pages/SandboxPage/SandboxPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./SandboxPage.module.css";
+
+const WordMenu = ({ coordinates }) => {
+  return <main className={classes.loading}>Loading...</main>;
+};
+
+export default WordMenu;
