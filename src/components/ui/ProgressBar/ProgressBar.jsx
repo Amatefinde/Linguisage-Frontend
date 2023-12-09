54097afd6711b3e90bc2eefa43f528cf94a5e18b
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-09 10:53:01 +0300

    add fix width add style props

--- src/components/ui/ProgressBar/ProgressBar.jsx
+++ src/components/ui/ProgressBar/ProgressBar.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 import classes from "./ProgressBar.module.css";
-const ProgressBar = ({ value, total}) => {
-    const percentage = (value / total) * 100;
+const ProgressBar = ({ value, total, style }) => {
+  const percentage = (value / total) * 100;
 
-    return (
-        <div className={classes.progressBar}>
-            <div className={classes.progressBackground}>
-                <div className={classes.progressLine} style={{ width: `${percentage}%`}}/>
-            </div>
-            <span className={classes.titleProgress}>{`${value}/${total}`}</span>
-        </div>
-    );
+  return (
+    <div className={classes.progressBar} style={style}>
+      <div className={classes.progressBackground}>
+        <div
+          className={classes.progressLine}
+          style={{ width: `${percentage}%` }}
+        />
+      </div>
+      <span className={classes.titleProgress}>{`${value}/${total}`}</span>
+    </div>
+  );
 };
 
 export default ProgressBar;
