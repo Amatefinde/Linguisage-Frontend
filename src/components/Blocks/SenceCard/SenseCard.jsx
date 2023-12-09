1ff9273f4578b258f9a4ab53259a1f239e3da1d0
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-09 10:52:43 +0300

    fix urls to img

--- src/components/Blocks/SenceCard/SenseCard.jsx
+++ src/components/Blocks/SenceCard/SenseCard.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 import classes from "./SenseCard.module.css";
 import ProgressBar from "../../ui/ProgressBar/ProgressBar";
+import capitalizeFirstLetter from "../../../utils/strings";
 
-const SenseCard = ({sense}) => {
-    return (
-        <div className={classes.senseCard}>
-            <div className={classes.senseBody}>
-                <div className={classes.senseWord}>
-                    {sense.word}
-                    <div className={classes.senseDefinition}>
-                        {sense.definition}
-                    </div>
-                </div>
-                <img src={sense.img} className={classes.senseImage} alt={sense.img.alt}/>
-            </div>
-            <ProgressBar value={sense.progress.value} total={sense.progress.total}/>
+const SenseCard = ({ sense }) => {
+  return (
+    <div className={classes.senseCard}>
+      <div className={classes.senseBody}>
+        <div className={classes.senseWord}>
+          {capitalizeFirstLetter(sense.word.word)}
+          <div className={classes.senseDefinition}>{sense.definition}</div>
         </div>
-    );
+        <img
+          src={"http://" + sense.images[0].img}
+          className={classes.senseImage}
+          alt={"Тут должна быть изображения"}
+        />
+      </div>
+      <ProgressBar
+        value={50 /*sense.progress.value*/}
+        total={100 /*sense.progress.total*/}
+      />
+    </div>
+  );
 };
 
 export default SenseCard;
