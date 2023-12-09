11338d105f1587fb0d5493a78fb7d815a2a2822f
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-09 20:31:55 +0300

    add placeholder for trying to send empty answer

--- src/components/Pages/TrainingPage/Exercises/DefineWordExercise/AnswerInput/AnswerInput.jsx
+++ src/components/Pages/TrainingPage/Exercises/DefineWordExercise/AnswerInput/AnswerInput.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./AnswerInput.module.css";
 
-const AnswerInput = ({ userAnswer, setUserAnswer }) => {
+const AnswerInput = ({ userAnswer, setUserAnswer, ...props }) => {
   return (
     <div>
       <input
+        {...props}
         className={classes.input}
         value={userAnswer ? userAnswer : ""}
         onChange={(e) => setUserAnswer(e.target.value)}
