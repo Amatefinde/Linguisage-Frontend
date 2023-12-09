3eeea579d9d8b424b8899133a4312349675742dc
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-09 20:30:50 +0300

    add full card open when click by word card

--- src/components/Pages/DictionaryPage/DictionaryPage.jsx
+++ src/components/Pages/DictionaryPage/DictionaryPage.jsx
@@ -3,6 +3,8 @@ import classes from "./DictionaryPage.module.css";
 import Header from "../../Blocks/Header/Header";
 import SenseCard from "../../Blocks/SenceCard/SenseCard";
 import WordService from "../../../services/WordService";
+import ModalFramer from "../../ui/ModalFramer/ModalFramer";
+import WordFullCard from "../../Blocks/WordFullCard/WordFullCard";
 
 const DictionaryPage = () => {
   const [userSenses, setUserSenses] = useState([]);
@@ -13,12 +15,23 @@ const DictionaryPage = () => {
     });
   }, []);
 
+  const [showModal, setShowModal] = useState(false);
+  const [openedSense, setOpenedSense] = useState(null);
+
   return (
     <>
       <Header />
+      <ModalFramer showModal={showModal} setShowModal={setShowModal}>
+        <WordFullCard sense={openedSense} />
+      </ModalFramer>
       <div className={classes.wrapper}>
         {userSenses.map((sense) => (
-          <SenseCard key={sense.id} sense={sense} />
+          <SenseCard
+            key={sense.id}
+            sense={sense}
+            setOpenedSense={setOpenedSense}
+            setShowModal={setShowModal}
+          />
         ))}
       </div>
     </>
