7918344df4d6d969a8b773bcdf0822882ad1c69f
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-10 22:23:24 +0300

    add style props

--- src/components/Pages/Loading/Loading.jsx
+++ src/components/Pages/Loading/Loading.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import classes from "./Loading.module.css";
 
-const Loading = () => {
-  return <main className={classes.loading}>Loading...</main>;
+const Loading = ({ style }) => {
+  return (
+    <main className={classes.loading} style={style}>
+      Loading...
+    </main>
+  );
 };
 
 export default Loading;
