e8320eeeca085f10929ae471dd1f55ca84e232fc
iRespectOnlyYen <MyNameIIIypuk@yandex.ru>
2023-12-11 14:55:58 +0300

    add question mark removing

--- src/utils/removePunctuationMarks.js
+++ src/utils/removePunctuationMarks.js
@@ -1,5 +1,5 @@
 export default function cleanText(text) {
   text = text.toLowerCase();
-  text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g, "");
   return text;
 }
