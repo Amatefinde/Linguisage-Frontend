0c2d02e4ed662743e08e2ee709e374ba60757753
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2023-12-26 11:17:50 +0300

    add separator

--- src/components/Blocks/AddWord/ImagesList/ImagesList.jsx
+++ src/components/Blocks/AddWord/ImagesList/ImagesList.jsx
@@ -1,31 +1,34 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import classes from "./ImagesList.module.css";
 import Image from "./Image/Image";
 import ImageColumn from "./ImageColumn/ImageColumn";
 import splitListByIndex from "../../../../utils/splitListByIndex";
-import { ActiveImagesContext } from "../AddWord";
+import {ActiveImagesContext} from "../AddWord";
+import {get_sense_from_wordContent_by_id} from "../utils";
 
-const ImagesList = ({ wordContent, activeSenseId }) => {
-  let left_images;
-  let right_images;
-  let images_or_error;
+const ImagesList = ({wordContent, activeSenseId}) => {
+    let left_images;
+    let right_images;
+    let images_or_error;
 
-  const images = wordContent?.senses?.filter(
-    (sense) => sense.id === activeSenseId,
-  )[0]?.images;
+    const images = get_sense_from_wordContent_by_id(wordContent, activeSenseId)?.images
 
-  if (images) {
-    [left_images, right_images] = splitListByIndex(images);
-    images_or_error = (
-      <>
-        <ImageColumn images={left_images}></ImageColumn>
-        <ImageColumn images={right_images}></ImageColumn>
-      </>
-    );
-  } else {
-    images_or_error = <h3>Not found 404</h3>;
-  }
-  return <div className={classes.imageList}>{images_or_error}</div>;
+    if (images) {
+        [left_images, right_images] = splitListByIndex(images);
+        images_or_error = (
+            <>
+                <ImageColumn images={left_images}></ImageColumn>
+                <ImageColumn images={right_images}></ImageColumn>
+            </>
+        );
+    } else {
+        images_or_error = <h3>Not found 404</h3>;
+    }
+    return (
+        <div>
+            <div className={classes.imageList}>{images_or_error}</div>
+            <div className={classes.separator}></div>
+        </div>);
 };
 
 export default ImagesList;
