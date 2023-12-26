404c572ca14f9e77ed358a2db75aa001c3a5fcde
AMDisPOWER <MyNameIIIyPuk@yandex.ru>
2023-12-26 11:18:11 +0300

    add check if word already added to user dictionary

--- src/components/Blocks/AddWord/AddWord.jsx
+++ src/components/Blocks/AddWord/AddWord.jsx
@@ -1,7 +1,7 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, {createContext, useContext, useEffect, useState} from "react";
 import classes from "./AddWord.module.css";
 import Search from "../../ui/Search/Search";
-import { ApplicationContext } from "../../../App";
+import {ApplicationContext} from "../../../App";
 import WordService from "../../../services/WordService";
 import cleanText from "../../../utils/removePunctuationMarks";
 import SensesList from "./SensesList/SensesList";
@@ -10,85 +10,86 @@ import Loading from "../../Pages/Loading/Loading";
 import FilledButton from "../../ui/Buttons/FilledButton/FilledButton";
 import EmptyButton from "../../ui/Buttons/EmptyButton/EmptyButton";
 import getTrueFields from "../../../utils/getTrueFileds";
+import Buttons from "./Buttons/Buttons";
+import {get_sense_from_wordContent_by_id} from "./utils";
 
 export const ActiveImagesContext = createContext(null);
 
-const AddWord = ({ setModalActive }) => {
-  const { currentWord, setCurrentWord, currentContext } =
-    useContext(ApplicationContext);
-  const [wordContent, setWordContent] = useState({});
-  const [activeSenseId, setActiveSenseId] = useState(null);
-  const [query, setQuery] = useState("");
-  const [activeImagesId, setActiveImagesId] = useState({});
+const AddWord = ({setModalActive}) => {
+    const {currentWord, setCurrentWord, currentContext} = useContext(ApplicationContext);
+    const [wordContent, setWordContent] = useState({});
+    const [activeSenseId, setActiveSenseId] = useState(null);
+    const [query, setQuery] = useState("");
+    const [activeImagesId, setActiveImagesId] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
-  const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        let cleanedText;
+        if (currentWord.length === 1) {
+            cleanedText = cleanText(currentWord[0].text);
+        } else {
+            cleanedText = currentWord.map((e) => cleanText(e.text)).join(" ");
+        }
+        setQuery(cleanedText);
+        const textContext = currentContext
+            .map((obj) => cleanText(obj.text))
+            .join(" ");
 
-  useEffect(() => {
-    let cleanedText;
-    if (currentWord.length === 1) {
-      cleanedText = cleanText(currentWord[0].text);
-    } else {
-      cleanedText = currentWord.map((e) => cleanText(e.text)).join(" ");
-    }
-    setQuery(cleanedText);
-    const textContext = currentContext
-      .map((obj) => cleanText(obj.text))
-      .join(" ");
+        WordService.getWord(cleanedText, textContext)
+            .then((fetchedWordContent) => {
+                setWordContent(fetchedWordContent);
+                setActiveSenseId(fetchedWordContent.current_sense_id);
 
-    WordService.getWord(cleanedText, textContext)
-      .then((fetchedWordContent) => {
-        setWordContent(fetchedWordContent);
-        console.log(fetchedWordContent.current_sense_id);
-        setActiveSenseId(fetchedWordContent.current_sense_id);
-      })
-      .catch((e) => console.log(e)) // todo
-      .finally(() => setIsLoading(false));
-  }, [currentWord]);
+            })
+            .catch((e) => console.log(e)) // todo
+            .finally(() => setIsLoading(false));
+    }, [currentWord]);
 
-  const imageList = (
-    <ImagesList
-      wordContent={wordContent}
-      activeSenseId={activeSenseId}
-    ></ImagesList>
-  );
-
-  function addHandler() {
-    const images_id = getTrueFields(activeImagesId);
-    WordService.addSenseToMe(images_id, activeSenseId).then(
-      setModalActive(false),
-    );
-  }
-
-  return (
-    <ActiveImagesContext.Provider
-      value={{
-        activeImagesId,
-        setActiveImagesId,
-      }}
-    >
-      <div className={classes.mainWidget}>
-        <div className={classes.leftSide}>
-          <Search value={query} setValue={setQuery} />
-          <SensesList
+    const imageList = (
+        <ImagesList
             wordContent={wordContent}
             activeSenseId={activeSenseId}
-            setActiveSenseId={setActiveSenseId}
-          />
-        </div>
-        <div className={classes.rightSide}>
-          <div className={classes.imagesListWrapper}>
-            {isLoading ? <Loading /> : imageList}
-          </div>
-          <div className={classes.buttonsWrapper}>
-            <EmptyButton onClick={() => setModalActive(false)}>
-              Cancel
-            </EmptyButton>
-            <FilledButton onClick={addHandler}>Add</FilledButton>
-          </div>
-        </div>
-      </div>
-    </ActiveImagesContext.Provider>
-  );
+        ></ImagesList>
+    );
+
+    const isAlreadyInDictionary = get_sense_from_wordContent_by_id(wordContent, activeSenseId)?.user_have
+
+    function addHandler() {
+        const images_id = getTrueFields(activeImagesId);
+        WordService.addSenseToMe(images_id, activeSenseId).then(
+            setModalActive(false),
+        );
+    }
+
+    return (
+        <ActiveImagesContext.Provider
+            value={{
+                activeImagesId,
+                setActiveImagesId,
+            }}
+        >
+            <div className={classes.mainWidget}>
+                <div className={classes.leftSide}>
+                    <Search value={query} setValue={setQuery}/>
+                    <SensesList
+                        wordContent={wordContent}
+                        activeSenseId={activeSenseId}
+                        setActiveSenseId={setActiveSenseId}
+                    />
+                </div>
+                <div className={classes.rightSide}>
+                    <div className={classes.imagesListWrapper}>
+                        {isLoading ? <Loading/> : imageList}
+                    </div>
+                    <Buttons
+                        setModalActive={setModalActive}
+                        addHandler={addHandler}
+                        isAlreadyInDictionary={isAlreadyInDictionary}
+                    />
+                </div>
+            </div>
+        </ActiveImagesContext.Provider>
+    );
 };
 
 export default AddWord;
