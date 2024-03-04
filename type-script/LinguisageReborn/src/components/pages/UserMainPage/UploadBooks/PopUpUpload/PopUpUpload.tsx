f121b1f4bcb8656d0572845f3b5f292b5e35a91b
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-03-04 12:56:30 +0300

    add book preview for epubs

--- type-script/LinguisageReborn/src/components/pages/UserMainPage/UploadBooks/PopUpUpload/PopUpUpload.tsx
+++ type-script/LinguisageReborn/src/components/pages/UserMainPage/UploadBooks/PopUpUpload/PopUpUpload.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState, ChangeEvent} from "react";
 import classes from "./PopUpUpload.module.css";
 import Input from "@mui/joy/Input";
 import Button from "@mui/joy/Button";
@@ -6,34 +6,62 @@ import {Checkbox, FormLabel} from "@mui/joy";
 import FormControl from "@mui/joy/FormControl";
 import FormHelperText from "@mui/joy/FormHelperText";
 import ButtonGroup from "@mui/joy/ButtonGroup";
+import ePub, {Book} from "epubjs";
 
+interface PopUpUploadProps {
+    file: File;
+    fileName: string;
+    setFileName: (value: string) => void;
+    hiddenFileInput: React.RefObject<HTMLInputElement>;
+    setIsModalActive: (value: boolean) => void;
+}
 
-const PopUpUpload = ({file, fileName, setFileName, hiddenFileInput, setIsModalActive}) => {
+const PopUpUpload: React.FC<PopUpUploadProps> = ({
+                                                     file,
+                                                     fileName,
+                                                     setFileName,
+                                                     hiddenFileInput,
+                                                     setIsModalActive
+                                                 }) => {
+    const [coverUrl, setCoverUrl] = useState<string | null>(null);
+    const book: Book = ePub(file);
     
-    useEffect(() => { // при демониторивании копмпонента сбрасываем текущую выбранные лит-ру
-        return () => hiddenFileInput.current.value = null
-    }, []);
+    book.coverUrl().then((cover) => setCoverUrl(cover));
+    
+    const handleFileNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFileName(e.target.value);
+    };
     
     return (
         <div className={classes.wrapper}>
-            <div className={classes.bookCover}>
-            </div>
+            {coverUrl && <img src={coverUrl} alt={file.name} className={classes.bookCover}/>}
             <div className={classes.forms}>
                 <section className={classes.management}>
                     <FormControl>
                         <FormLabel>Title</FormLabel>
-                        <Input value={file?.text && fileName} onChange={e => setFileName(e)}/>
-                    
+                        <Input value={file?.name && fileName} onChange={handleFileNameChange}/>
                     </FormControl>
                     <FormControl>
                         <Checkbox label="Automatic text recognition" disabled/>
                         <FormHelperText>Only for PDF</FormHelperText>
                     </FormControl>
-                
                 </section>
                 <ButtonGroup spacing={2}>
-                    <Button variant={"outlined"} color="neutral" onClick={() => setIsModalActive(false)}>Cancel</Button>
-                    <Button variant={"solid"} color="primary">Upload</Button>
+                    <Button
+                        variant={"outlined"}
+                        color="neutral"
+                        onClick={() => {
+                            setIsModalActive(false);
+                            if (hiddenFileInput.current) {
+                                hiddenFileInput.current.value = "";
+                            }
+                        }}
+                    >
+                        Cancel
+                    </Button>
+                    <Button variant={"solid"} color="primary">
+                        Upload
+                    </Button>
                 </ButtonGroup>
             </div>
         </div>
--- type-script/LinguisageReborn/src/components/pages/UserMainPage/UploadBooks/UploadBooks.tsx
+++ type-script/LinguisageReborn/src/components/pages/UserMainPage/UploadBooks/UploadBooks.tsx
@@ -3,11 +3,16 @@ import classes from "./UploadBooks.module.css";
 import PopUpUpload from "./PopUpUpload/PopUpUpload";
 import Loading from "../../../blocks/Loading/Loading.js";
 import UploadWidget from "./UploadWidget/UploadWidget";
-import Modal from "../../../ui/Modal/Modal";
+import {Transition} from 'react-transition-group';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import DialogTitle from '@mui/joy/DialogTitle';
+import DialogContent from '@mui/joy/DialogContent';
+import {current} from "@reduxjs/toolkit";
 
 
 const UploadBooks = () => {
-    const [isModalActive, setIsModalActive] = useState<boolean>(false);
+    const [open, setOpen] = useState<boolean>(false);
     const [file, setFile] = useState<File | null>(null);
     const [fileName, setFileName] = useState<string | null>(null)
     const [isFileLoading, setIsFileLoading] = useState<boolean>(false);
@@ -27,28 +32,70 @@ const UploadBooks = () => {
     
     return (
         <>
-            <div style={{zIndex: "20"}}>
-            <Modal showModal={isModalActive} setShowModal={setIsModalActive}>
-                <PopUpUpload
-                    file={file}
-                    fileName={fileName}
-                    setFileName={setFileName}
-                    callback={uploadFile}
-                    hiddenFileInput={hiddenFileInput}
-                    setIsModalActive={setIsModalActive}
-                />
-            </Modal>
+            <Transition in={open} timeout={400}>
+                {(state: string) => (
+                    <Modal
+                        keepMounted
+                        open={!['exited', 'exiting'].includes(state)}
+                        onClose={() => {
+                            setOpen(false)
+                        }}
+                        slotProps={{
+                            backdrop: {
+                                sx: {
+                                    opacity: 0,
+                                    backdropFilter: 'none',
+                                    transition: `opacity 200ms, backdrop-filter 400ms`,
+                                    ...{
+                                        entering: {opacity: 1, backdropFilter: 'blur(8px)', background: "rgba(0,0,0,0.5)"},
+                                        entered: {opacity: 1, backdropFilter: 'blur(8px)', background: "rgba(0,0,0,0.5)"},
+                                    }[state],
+                                },
+                            },
+                        }}
+                        sx={{
+                            visibility: state === 'exited' ? 'hidden' : 'visible',
+                            
+                        }}
+                    >
+                        <ModalDialog
+                            sx={{
+                                opacity: 0,
+                                borderRadius: "20px",
+                                transition: `opacity 200ms`,
+                                ...{
+                                    entering: {opacity: 1, y: "100"},
+                                    entered: {opacity: 1,  y: "0"},
+                                }[state],
+                            }}
+                        >
+                            <DialogContent>
+                                    <PopUpUpload
+                                        file={file}
+                                        fileName={fileName}
+                                        setFileName={setFileName}
+                                        callback={uploadFile}
+                                        hiddenFileInput={hiddenFileInput}
+                                        setIsModalActive={setOpen}
+                                    />
+                            </DialogContent>
+                        </ModalDialog>
+                    </Modal>
+                )}
+            </Transition>
+            {/*<Modal showModal={isModalActive} setShowModal={setIsModalActive}>*/}
+
+            {/*</Modal>*/}
             {isFileLoading ? (
                 <Loading/>
             ) : (
                 <UploadWidget
-                    setIsModalActive={setIsModalActive}
+                    setIsModalActive={setOpen}
                     setFile={setFile}
                     setFileName={setFileName}
                     hiddenFileInput={hiddenFileInput}
                 />
             )}
-            </div>
         </>
     );
 };
