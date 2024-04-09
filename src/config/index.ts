b22500054173bb962993f826934153f13428be33
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-04-09 12:05:45 +0300

    return api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 
 
-// export const API_URL: string = "https://api.linguisage.ru:9300/api/v1/";
-export const API_URL: string = "http://127.0.0.1:8002/api/v1/";
+export const API_URL: string = "https://api.linguisage.ru:9300/api/v1/";
+// export const API_URL: string = "http://127.0.0.1:8002/api/v1/";
