945d661e877bc829a2e62f84ba6ad734f0876028
Amatefinde <gurov@tm-serv.ru>
2025-03-01 15:43:37 +0300

    update backend url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 
 
-export const API_URL: string = "https://linguisage.ru:9300/api/v1/";
+export const API_URL: string = "https://linguisage.fun:9300/api/v1/";
 // export const API_URL: string = "http://127.0.0.1:8002/api/v1/";
